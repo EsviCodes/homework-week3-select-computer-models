84c3ac4c1b860560ed3ee5f0b0aed76e4852fffd
EsviCodes <esther.js.vis@gmail.com>
2019-10-05 10:30:05 +0200

    Connected App Component with Redux State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./App.css";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+class App extends React.Component {
+  // console.log(props); // check dispatch when App was a function - works!
+
+  render() {
+    // console.log(this.props); // check dispatch when App is a class - works!
+    // console.log(this.props); // check if computerModels is a prop - works!
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    computerModels: state.computerModels
+  };
+};
+
+export default connect(mapStateToProps)(App);
