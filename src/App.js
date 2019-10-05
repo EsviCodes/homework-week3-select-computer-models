5d2322508ef20db6966ff51cc3b04bb7be155cdf
EsviCodes <esther.js.vis@gmail.com>
2019-10-05 15:43:50 +0200

    Added button and method to dispatch chosen computer model to Redux State

--- src/App.js
+++ src/App.js
@@ -2,10 +2,39 @@ import React, { Component } from "react";
 import "./App.css";
 import { connect } from "react-redux";
 
+// SEE IF YOU CAN PREVENT DEFAULT WHILE HANDLING EVENTS!
+
 class App extends Component {
   // console.log(props); // check dispatch when App was a function - works!
 
-  // Local State
+  data = [
+    {
+      name: "Ivel Z3",
+      manufacturer: "Ivasim",
+      year: 1969,
+      origin: "Croatia"
+    },
+    {
+      name: "Bally Astrocade",
+      manufacturer: "Bally Consumer Products",
+      year: 1977,
+      origin: "USA"
+    },
+    {
+      name: "Sord M200 Smart Home Computer",
+      manufacturer: "Sord Computer Corporation",
+      year: 1971,
+      origin: "Japan"
+    },
+    {
+      name: "Commodore 64",
+      manufacturer: "Commodore",
+      year: 1982,
+      origin: "USA"
+    }
+  ];
+
+  // Local State - stores currently selected model
   state = {};
 
   // handleSubmit(event) {
@@ -14,42 +43,45 @@ class App extends Component {
   //   event.preventDefault();
   // }
 
-  // Event Handler
+  // Event Handlers
   updateSelection = event => {
-    console.log("I Changed"); // works
-    console.log(event.target.value); // works
-    this.setState({ name: event.target.value });
+    // console.log("I Changed"); // works
+    // console.log(event.target.value); // works
+
+    // CHECK-this overrides the name property. Should uses like this or should it create an array of objects?
+    this.setState({
+      name: event.target.value
+    });
   };
+
+  addModel = () =>
+    // console.log("click"); // works!
+    // console.log(event); // works. gets event
+    // console.log(this.data); // works! gives array with objects
+
+    this.data.map(model => {
+      // console.log(model) // works!
+      // console.log(model.name) // works!
+
+      if (model.name === this.state.name) {
+        const action = {
+          type: "ADD_MODEL",
+          payload: {
+            name: model.name,
+            manufacturer: model.manufacturer,
+            year: model.year,
+            origin: model.origin
+          }
+        };
+
+        this.props.dispatch(action); // works. Shows in the Redux Dev tools
+      }
+    });
 
   render() {
     // console.log(this.props); // check dispatch when App is a class - works!
     // console.log(this.props); // check if computerModels is a prop - works!
-    const data = [
-      {
-        name: "Ivel Z3",
-        manufacturer: "Ivasim",
-        year: 1969,
-        origin: "Croatia"
-      },
-      {
-        name: "Bally Astrocade",
-        manufacturer: "Bally Consumer Products",
-        year: 1977,
-        origin: "USA"
-      },
-      {
-        name: "Sord M200 Smart Home Computer",
-        manufacturer: "Sord Computer Corporation",
-        year: 1971,
-        origin: "Japan"
-      },
-      {
-        name: "Commodore 64",
-        manufacturer: "Commodore",
-        year: 1982,
-        origin: "USA"
-      }
-    ];
+
     // Generates an unique identifier - use in key
     const uuidv4 = require("uuid/v4");
 
@@ -62,7 +94,7 @@ class App extends Component {
           <option key={uuidv4()} value="">
             -- pick a model --
           </option>{" "}
-          {data.map(computer => {
+          {this.data.map(computer => {
             // console.log(computer) // gives an object of a computer model
             // console.log("NAME", computer.name); // gives computer name
             // console.log("YEAR", computer.year); // gives year of the computer model
@@ -73,6 +105,7 @@ class App extends Component {
             );
           })}
         </select>
+        <button onClick={this.addModel}>Add</button>
       </div>
     );
   }
